84e2cb0ec2551f02ddb629cca79fe20d4575317f
AntonioMoralesCaldero <antoniommoralescaldero@gmail.com>
2024-04-15 13:53:51 +0200

    Initial commit

---
+++ src/main/java/com/example/demo/Ra3Ra7MoralesCalderoAntonioMiguelApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ra3Ra7MoralesCalderoAntonioMiguelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ra3Ra7MoralesCalderoAntonioMiguelApplication.class, args);
+	}
+
+}
